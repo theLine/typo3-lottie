bc238706c6a80a4e5801eb2e59e90056acdb8873
theLine <github@theline.capella.uberspace.de>
2019-08-02 22:21:38 +0200

    [TASK] Add required extension files

---
+++ ext_emconf.php
@@ -0,0 +1,26 @@
+<?php
+/** @var string $_EXTKEY */
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'Lottie',
+	'description' => 'Extension to enable rendering of Lottie/Bodymovin animations in frontend.',
+	'category' => 'fe',
+	'version' => '1.0.0',
+	'state' => 'alpha',
+	'clearcacheonload' => true,
+	'author' => 'theLine',
+	'author_email' => 'typo3@theline.capella.uberspace.de',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '8.7.0-9.5.999',
+		],
+		'conflicts' => [
+		],
+		'suggests' => [
+		],
+	],
+	'autoload' => [
+		'psr-4' => [
+			'TheLine\\Lottie\\' => 'Classes',
+		],
+	],
+];
