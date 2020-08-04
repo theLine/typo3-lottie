5ddc9899ec6fd8b9d1550c2e1783ae7cd14c5c94
theLine <git@theline.capella.uberspace.de>
2020-08-04 19:59:45 +0200

    [BUGFIX] Properly add support for TYPO3 CMS v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = [
 	'title' => 'Lottie',
 	'description' => 'Extension to enable rendering of Lottie/Bodymovin animations in frontend.',
 	'category' => 'fe',
-	'version' => '1.0.7',
+	'version' => '1.0.8',
 	'state' => 'beta',
 	'clearcacheonload' => true,
 	'author' => 'theLine',
 	'author_email' => 'typo3@theline.capella.uberspace.de',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '8.7.0-9.5.999',
+			'typo3' => '8.7.0-10.4.99',
 		],
 		'conflicts' => [
 		],
