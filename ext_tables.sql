6dd3ba78e75af44e69e786350f0cf78a8d037462
theLine <git@theline.uber.space>
2020-11-21 15:12:02 +0100

    [CLEANUP] Add proper comment for ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,5 +1,5 @@
 #
-# Table structure for table 'sys_file_reference'
+# Table structure for table 'sys_file_metadata'
 #
 CREATE TABLE sys_file_metadata (
 	tx_lottie_is_lottie_animation tinyint(1) DEFAULT '0' NOT NULL
